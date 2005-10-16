2656bacda4bf7d6a2401ed86ab9cfcb7bf24a44d
nwsmith <nwsmith>
2005-10-16 22:36:52 +0000

    Initial revision of unit test for the Board class

---
+++ test/test_board.rb
@@ -0,0 +1,30 @@
+#
+#   $Id$
+#
+#   Copyright 2005 Nathan Smith, Sheldon Fuchs
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+$:.unshift File.join(File.dirname(__FILE__), "..", "src")
+
+require "src/board"
+require "test/unit"
+
+class TestBoard < Test::Unit::TestCase
+    def test_init
+        b = Board::Board.new(2);
+        assert_equal(b.sq_at(Board::Coord.new(0,0)).colour, "black")
+        assert_equal(b.sq_at(Board::Coord.new(0,1)).colour, "white")
+        assert_equal(b.sq_at(Board::Coord.new(1,0)).colour, "white")
+        assert_equal(b.sq_at(Board::Coord.new(1,1)).colour, "black")                        
+    end
+end
