ee0da363b90e9c6db997e4d16a63cf3d2721ffa9
shelfoo <shelfoo>
2005-10-18 06:22:02 +0000

    Fix tabs - replace with spaces

--- src/move.rb
+++ src/move.rb
@@ -17,28 +17,28 @@
 #
 module Move
     class MoveNode
-	attr_accessor :white_ply
-	attr_accessor :black_ply
-	attr_accessor :next
+        attr_accessor :white_ply
+        attr_accessor :black_ply
+        attr_accessor :next
 
-	:children
+        :children
 
-	def initialize
-	    @children = Array.new()
-	end
+        def initialize
+            @children = Array.new()
+        end
 
-	def add_child(node)
-	    @children.append(node)
-	end
+        def add_child(node)
+            @children.append(node)
+        end
     end
 
     class Ply
-	attr_reader :source
-	attr_reader :destination
+        attr_reader :source
+        attr_reader :destination
 
-	def initialize(source, destination)
-	    @source = source
-	    @destination = destination
-	end
+        def initialize(source, destination)
+            @source = source
+            @destination = destination
+        end
     end
 end
--- src/piece.rb
+++ src/piece.rb
@@ -17,34 +17,34 @@
 #
 module ChessPiece
     class ChessPiece
-	attr_reader :color
-	attr_reader :value
-	attr_reader :name
-	:state
+        attr_reader :color
+        attr_reader :value
+        attr_reader :name
+        :state
 
-	def initialize(color, value, name)
-	    @color = color
-	    @value = value
-	    @name = name
-	    @state = PieceState.new
-	end
+        def initialize(color, value, name)
+            @color = color
+            @value = value
+            @name = name
+            @state = PieceState.new
+        end
     end
 
     class PieceState
-	attr_accessor :moved
-	attr_accessor :captured
+        attr_accessor :moved
+        attr_accessor :captured
 
-	def initialize 
-	    @moved = false
-	    @captured = false
-	end
+        def initialize 
+            @moved = false
+            @captured = false
+        end
 
-	def is_moved?
-	    return @moved
-	end
+        def is_moved?
+            return @moved
+        end
 
-	def is_captured?
-	    return @captured
-	end
+        def is_captured?
+            return @captured
+        end
     end
 end
