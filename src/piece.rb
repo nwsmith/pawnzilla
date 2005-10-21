661b1fe36c37e34384d81bf7448a53ab5827326a
nwsmith <nwsmith>
2005-10-21 07:08:19 +0000

    - Removed the value property - it is to be managed from the Engine

--- src/piece.rb
+++ src/piece.rb
@@ -18,13 +18,11 @@
 module ChessPiece
     class ChessPiece
         attr_reader :color
-        attr_reader :value
         attr_reader :name
         :state
 
-        def initialize(color, value, name)
+        def initialize(color, name)
             @color = color
-            @value = value
             @name = name
             @state = PieceState.new
         end
