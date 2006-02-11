4efbd113a91dd9073c2ca4ffad135176ef4ec090
nwsmith <nwsmith>
2006-02-11 21:19:00 +0000

    Initial bitboard implementation.  Current features are:
    - initialize all bitboard vectors with starting pieces.  Vectors are: one for white pieces, one for black pieces, and one for each piece type.
    - retrieve a square with proper colour and piece information from the bitboard

---
+++ src/bitboard.rb
@@ -0,0 +1,68 @@
+#
+#   $Id$
+#
+#   Copyright 2005 Nathan Smith, Sheldon Fuchs, Ron Thomas
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+require "chess"
+
+class Bitboard
+    # Each property it an individual bitboard
+    attr :blk_pc  # All black pieces
+    attr :wht_pc  # All white pieces
+    attr :p       # All pawns
+    attr :r       # All rooks
+    attr :n       # All knights
+    attr :b       # All bishops
+    attr :q       # All queens
+    attr :k       # All kings
+    
+    def initialize()         
+        @blk_pc = 0x00_00_00_00_00_00_FF_FF
+        @wht_pc = 0xFF_FF_00_00_00_00_00_00
+        
+        @p = 0x00_FF_00_00_00_00_FF_00
+        @r = 0x81_00_00_00_00_00_00_81
+        @n = 0x42_00_00_00_00_00_00_42
+        @b = 0x24_00_00_00_00_00_00_24
+        @q = 0x10_00_00_00_00_00_00_10
+        @k = 0x08_00_00_00_00_00_00_08
+    end
+    
+    def sq_at(coord) 
+        # First determine which bit in the vector this coord represents
+        # derived from (8 * (7 - y)) + (7 - x)
+        pos = 63 - (8 * coord.y) - coord.x
+        
+        # Look for a piece of either colour in that square
+        piece = nil;
+        color = @blk_pc[pos] == 1 ? Chess::Colour.new_black : @wht_pc[pos] ? Chess::Colour.new_white : nil;
+
+        # Determine piece type
+        if !color.nil?
+            piece = Chess::Piece.new(color, "Pawn") if @p[pos] == 1
+            piece = Chess::Piece.new(color, "Rook") if @r[pos] == 1
+            piece = Chess::Piece.new(color, "Knight") if @n[pos] == 1
+            piece = Chess::Piece.new(color, "Bishop") if @b[pos] == 1
+            piece = Chess::Piece.new(color, "Queen") if @q[pos] == 1
+            piece = Chess::Piece.new(color, "King") if @k[pos] == 1
+        end
+
+        square = Chess::Square.new(coord, Chess::Board.get_colour(coord));
+        
+        square.piece = piece if ! piece.nil?
+        
+        square
+    end
+end
