84141edef8f0c435c8494d847ea8c8edcd278613
nwsmith <nwsmith>
2006-08-07 19:17:10 +0000

    - Added bitwise or operator

--- src/bitboard.rb
+++ src/bitboard.rb
@@ -46,6 +46,10 @@ class Bitboard
         return @bv & bv
     end
     
+    def |(bv) 
+        return @bv | bv
+    end
+    
     def to_s
 		out = ""
 		63.downto(0) do |i|
--- test/tc_bitboard.rb
+++ test/tc_bitboard.rb
@@ -79,4 +79,15 @@ class BitboardTest < Test::Unit::TestCase
         bitboard &= 0x1
         assert_equal(0x1 & 0x1, bitboard)
     end
+    
+    def test_bitwise_or
+        bitboard = Bitboard.new(0x1) | 0x1
+        assert_equal(0x1 | 0x1, bitboard)
+    end
+    
+    def test_bitwise_or_equals
+        bitboard = Bitboard.new(0x1)
+        bitboard |= 0x1
+        assert_equal(0x1 | 0x1, bitboard)
+    end
 end
