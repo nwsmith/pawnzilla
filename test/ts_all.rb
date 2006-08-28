9f3c5eaf7067ceb42e56d305c3af506f3a8aa743
streiff <streiff>
2006-08-28 02:48:17 +0000

    changed to run all test files programmatically

--- test/ts_all.rb
+++ test/ts_all.rb
@@ -14,10 +14,33 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-#
+
+# This file must be ran from the test directory or its parent.
+# It runs all files beginning with the string tc_
+
+require "find"
+require "fileutils"
 require 'test/unit'
-require 'tc_geometry'
-require 'tc_chess'
-require 'tc_gamestate'
-require 'tc_move'
-require 'tc_translator'
+
+pwd = FileUtils.pwd()
+
+
+# Determine where we are
+test_dir = File.directory?(File.join("..", "test"))
+test_is_subdir = File.directory?("test")
+
+# change into the test directory or alert that we were run from an unknown location
+if (!test_dir && test_is_subdir) 
+    FileUtils.cd("test")
+elsif (!test_dir && !test_is_subdir)
+    fail("Ran from unexpected directory. Failing")
+end
+
+# Run all tests
+Find.find(FileUtils.pwd()) do |filename|
+    next if filename.match("[\.-]svn") # skip svn directories
+    next unless filename.split(File::SEPARATOR).last().match(/(tc_.*)\.rb/)
+    require $1
+end
+
+FileUtils.cd(pwd)
