4524fe47d785a1bea5979412605e6de66343a455
streiff <streiff>
2008-06-20 19:16:13 +0000

    Added installation instructions in the readme.
    Modified plugin to no longer crash jin on startup... although
    it still does when the server is selected.

--- support/jin/src/net/sourceforge/pawnzilla/jin/JinPlugin.java
+++ support/jin/src/net/sourceforge/pawnzilla/jin/JinPlugin.java
@@ -18,37 +18,102 @@
 
 package net.sourceforge.pawnzilla.jin;
 
-import free.jin.AbstractServer;
+import free.jin.Server;
 import free.jin.Connection;
 import free.jin.ConnectionDetails;
+import free.jin.User;
 import free.jin.UsernamePolicy;
 
 /** Communication class with Jin Chess client
  *
  * @author streiff
  */
-public class JinPlugin extends AbstractServer {
+public class JinPlugin implements Server {
 
-    @Override
-    protected UsernamePolicy createUsernamePolicy() {
-        return new UsernamePolicy() {
+    /** Pawnzilla doesn't need a server, but we must mock a username policy */
+    private static final UsernamePolicy usernamePolicy = new UsernamePolicy() {
+        public boolean isSame(String username1, String username2) {
+            return username1.equalsIgnoreCase(username2);
+        }
 
-            public boolean isSame(String username1, String username2) {
-                return username1.equalsIgnoreCase(username2);
-            }
+        public String invalidityReason(String username) {
+            return null;
+        }
 
-            public String invalidityReason(String username) {
-                return null;
-            }
+        public String getGuestUsername() {
+            return "guest";
+        }
+    };
+    
+    private User guestUser;
 
-            public String getGuestUsername() {
-                return "guest";
-            }
-        };
+    
+
+    public Connection createConnection(ConnectionDetails connDetails) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public Connection createConnection(ConnectionDetails arg0) {
+    public void setGuestUser(User user) {
+        guestUser = user != null 
+                ? user : new User(this, usernamePolicy.getGuestUsername());
+    }
+
+    public User getGuest() {
+        return guestUser;
+    }
+
+    public String getDefaultHost() {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String[] getHosts() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setHost(String arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int[] getPorts() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setPort(int arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getId() {
+        return "pawnzilla";
+    }
+
+    public String getShortName() {
+        return "Pawnzilla";
+    }
+
+    public String getLongName() {
+        return "Pawnzilla Chess Engine";
+    }
+
+    public String getWebsite() {
+        return "https://sourceforge.net/projects/pawnzilla";
+    }
+
+    public String getRegistrationPage() {
+        return "Local chess engine - no registration required.";
+    }
+
+    public String getPasswordRetrievalPage() {
+        return "Local chess engine - no password required";
+    }
+
+    
+    public UsernamePolicy getUsernamePolicy() {
+        return usernamePolicy;
+    }
+    
+    @Override
+    public String toString(){
+        return getLongName() + " (" + getWebsite() + ")";
     }
 
 }
