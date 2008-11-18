de51f336a970c36caac616b583bad47e1c461c69
nsmith <nsmith@10.0.1.197>
2008-11-17 21:30:20 -0700

    Adding $: modification to make sure it can find move_engine

--- engine/test/test_move_engine.rb
+++ engine/test/test_move_engine.rb
@@ -15,6 +15,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
+$:.unshift File.join(File.dirname(__FILE__), "..", "src")
+
 require "move_engine"
 
 class TestMoveEngine < MoveEngine
